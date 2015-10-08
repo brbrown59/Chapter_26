6c984399e480e7bb2bc4d803c1bb00805c351e93
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-08 15:59:57 -0600

    Started Chapter 26 project; created files for basic header, common functions,  homepage, and database setup.

---
+++ functions.php
@@ -0,0 +1,64 @@
+<?php
+	//all the functions used by other parts of the project
+
+	//DO NOT COMMIT THESE WITH VALUES ENTERED!!!!
+	//log-in details for database
+	$dbhost = 'localhost';
+	$dbname = '';
+	$dbuser = '';
+	$dbpass = '';
+	$appname = "The Project";
+
+	//create connection to database
+	$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+	if ($connection ->connect_error) die($connection->connect_error);
+
+	//called to create new tables in the database
+	function createTable ($name, $query)
+	{
+		queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+		echo "Table $name created or already exists.<br>";
+	}
+
+	//called to make queries to the database
+	function queryMysql($query)
+	{
+		global $connection;
+		$result = $connection->query($query);
+		if(!$result) die ($connection->error);
+		return $result;
+
+	}
+
+	//destroy created sessions and cookies
+	function destroySession()
+	{
+		$_SESSION = array();
+		if (session_id() != "" || isset($_COOKIE[session_name()]))
+			setcookie(session_name(), '', time()-2592000, '/');
+		session_destroy();
+	}
+
+	//sanitize any inputs
+	function santitizeString($var)
+	{
+		global $connection;
+		$var = strip_tags($var);
+		$var = htmlentities($var);
+		$var = stripslashes($var);
+		return $connection->real_escape_string($var);
+	}
+
+	//fetches profile of given user from database
+	function showProfile ($user)
+	{
+		if (file_exists("user.jpg"))
+			echo "<img src='user.jpg' style='float:left;>";
+		$result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+		if ($result->num_rows)
+		{
+			$row = $result->fetch_array(MYSQLI_ASSOC);
+			echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+		}
+	}
+?>
---
+++ header.php
@@ -0,0 +1,51 @@
+<?php
+
+	//set up session; will remember values to be shared across files
+	session_start();
+
+	//
+	echo "<!DOCTYPE html>\n<html><head>";
+
+	//makes sure all files have access to the functions
+	require_once 'functions.php';
+
+	$userstr = ' (Guest)';
+
+	//check if a user is logged in by checking if the session ID variable has a value
+	if (isset($_SESSION['user']))
+	{
+		$user = $_SESSION['user'];
+		$loggedin = TRUE;
+		$userstr = " ($user)";
+	}
+	else $loggedin = FALSE;
+
+//loads style sheet, creates canvas element for logo and loads javascript file
+	echo "<title>$appname$userstr</title><link rel='stylesheet' " .
+		"href='styles.css' type='text/css'>"                     .
+		"</head><body><center><canvas id='logo' width='624' "    .
+		"height='96'>$appname</canvas></center>"             .
+		"<div class='appname'>$appname$userstr</div>"            .
+		"<script src='javascript.js'></script>";
+
+	//present different options depending if user is logged in or not
+	if ($loggedin)
+	{
+		echo "<br ><ul class='menu'>" .
+			"<li><a href='members.php?view=$user'>Home</a></li>" .
+			"<li><a href='members.php'>Members</a></li>"         .
+			"<li><a href='friends.php'>Friends</a></li>"         .
+			"<li><a href='messages.php'>Messages</a></li>"       .
+			"<li><a href='profile.php'>Edit Profile</a></li>"    .
+			"<li><a href='logout.php'>Log out</a></li></ul><br>";
+	}
+	else
+	{
+		echo ("<br><ul class='menu'>" .
+			"<li><a href='index.php'>Home</a></li>"                .
+			"<li><a href='signup.php'>Sign up</a></li>"            .
+			"<li><a href='login.php'>Log in</a></li></ul><br>"     .
+			"<span class='info'>&#8658; You must be logged in to " .
+			"view this page.</span><br><br>");
+	}
+?>
